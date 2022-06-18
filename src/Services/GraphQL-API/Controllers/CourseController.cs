2120e22e43af5c2b2cecc741408a272282e04309
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-06-18 02:15:59 +0200

    Added Query and logic for GetCourses

--- src/Services/GraphQL-API/Controllers/CourseController.cs
+++ src/Services/GraphQL-API/Controllers/CourseController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GraphQL.SystemTextJson;
+using GraphQL.Types;
+using GraphQL_API.Models;
+using GraphQL_API.Queries;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GraphQL_API.Controllers;
 
@@ -6,5 +10,50 @@ namespace GraphQL_API.Controllers;
 [ApiController]
 public class CourseController : ControllerBase
 {
+    private readonly ILogger<CourseController> _logger;
 
+    public CourseController(ILogger<CourseController> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpGet]
+    [Route("getcourses")]
+    public async Task<string> GetCourses([FromQuery] string query)
+    {
+        var schema = Schema.For(@"
+                    type Query {
+                                   courses : [Course!]  
+                                   course (id : ID!) : Course
+                                 }
+                    enum PaymentType {
+                                        FREE ,
+                                        PAID
+                                  }
+                    type Course {
+                                    title: String!
+                                    duration: Int
+                                    level : String!
+                                    instructor: String
+                                    paymentType : PaymentType
+                                    ratings : [Rating]
+                                }
+                    type Rating
+                                {   
+                                    studentName : String
+                                    stars : Int
+                                    review : String
+                                }", builder =>
+        {
+            builder.Types.Include<Course>();
+            builder.Types.Include<Query>();
+        });
+
+        var json = await schema.ExecuteAsync(options =>
+        {
+            options.Query = query;
+        });
+
+        return json;
+    }
 }
